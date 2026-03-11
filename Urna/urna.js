18dda07121095852167df4de018cfec125331d0d
xandeelias <alexandrencarr1@gmail.com>
2026-03-11 16:40:41 -0300

    feat: Calculadora Terminada e Iniciada Urna Eletrônica

---
+++ Urna/urna.js
@@ -0,0 +1,16 @@
+function urna(){
+    let candidato_a = 0;
+    let candidato_b = 0;
+    let brancos = 0;
+    let nulos = 0;
+    let total = 0;
+    let porcent_cand_a;
+    let porcent_cand_b;
+    let porcent_brancos;
+    let porcent_nulos;
+    let voto;
+    
+    do{
+
+    } while(voto != 0);
+}
